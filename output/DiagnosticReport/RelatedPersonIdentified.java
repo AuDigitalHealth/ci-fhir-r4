0bd0c4ed5c20a76c5dbab819fe63c0efe2c6edd0
Danielle Tavares-Rixon <danielle.tavares-rixon@digitalhealth.gov.au>
2021-02-02 15:18:41 +1000

    DiagnosticReport - inclusion of Identified Patient & RelatedPerson
    Update of ServiceRequest profile to allow for requester to be a Patient and a RelatedPerson

---
+++ output/DiagnosticReport/RelatedPersonIdentified.java
@@ -0,0 +1,7 @@
+package http://ns.electronichealth.net.au/ci/fhir/4.0/ImplementationGuide/implementationguide-diagnosticreport-1;
+
+import org.hl7.fhir.r5.model.ProfilingWrapper;
+
+public class RelatedPersonIdentified {
+
+}
